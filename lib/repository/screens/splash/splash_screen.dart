88e706e3704d2ee4a9aa0ad2f2fea6e9e0807b54
ahmed07raxa <ahmedgoofy6@gmail.com>
2026-06-21 12:18:33 +0500

    Dummy Splash Screen

---
+++ lib/repository/screens/splash/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:spotify_clone/repository/screens/home/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [Center(child: Text("SPLASH SCREEN"))]),
+    );
+  }
+}
